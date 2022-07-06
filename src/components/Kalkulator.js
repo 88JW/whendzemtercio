a0a8dcb1d592a58bab59906a1066497d02471dda
Wojciech Jaskula <w.jaskula8@gmail.com>
2022-07-06 14:14:17 +0200

    master

--- src/components/Kalkulator.js
+++ src/components/Kalkulator.js
@@ -10,26 +10,95 @@ export default class Kalkulator extends Component {
 
     state = {
 
-        truskawkicena: '',
-        cukiercena: '3.20',
-        energiacena: '0.74',
-        czascena: '2',
-        moccena: '1',
+        truskawkicena: 10,
+        cukiercena: 3.20,
+        energiacena: 0.74,
+        owocecena: 1,
+        czascena: 2,
+        moccena: 1,
+        sloiki: false,
+
     }
 
     handleChange = (e) => {
-        console.log(e.target.type);
-        console.log(e.target.name);
-        console.log(e.target.value);
-        // const name = e.target.name;
-        const value = e.target.value;
+
+
+
+
+
+        // console.log(e.target.type);
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+        // console.log(this.state.cukiercena.type);
         const name = e.target.name;
-        this.setState({
-            // truskawkicena: value,
-            [name]: value,
-        })
+        const type = e.target.type;
+        const checked = e.target.checked;
+
+        if (type === "number") {
+
+            const value = e.target.value;
+            this.setState({
+                [name]: value,
+            })
+        }
+        else if (type === "checkbox") {
+
+            const checked = e.target.checked;
+            console.log(checked);
+            this.setState({
+
+                [name]: checked,
+            })
+        }
+    }
+
+    handleSubmmit = (e) => {
+
+        e.preventDefault()
+        console.log("obliczanie");
+        console.log(this.state.truskawkicena);
+        console.log(this.state.cukiercena);
+        console.log(this.state.cukiercena);
+
+
+        const wynik = (Number((this.state.truskawkicena) * this.state.owocecena) + ((this.state.cukiercena / 2) * this.state.owocecena) + ((1 * this.state.energiacena) * this.state.czascena));
+
+
+
+
+        if (this.state.sloiki === true) {
+            // console.log('możesz nie kupować słoików');
+            console.log((wynik) / (4 * this.state.owocecena) + "za słoiczek 200ml dżemu");
+        }
+
+        else {
+            console.log('należy doliczyć jeszcze cenę słokików');
+            console.log(wynik + (1.8 * (4 * this.state.owocecena)));
+            // console.log((wynik) / (1.8 * (4 * this.state.owocecena)) + "za słoiczek 200ml dżemu")
+            console.log('cena za słoik');
+            console.log((wynik + (1.8 * (4 * this.state.owocecena))) / (4 * this.state.owocecena));
+
+
+
+        }
+
+
+        // console.log(wynik);
+
+
+
+
+
+        // if (true) {
+
+        //     const wynik = this.setState.truskawkicena;
+
+        //     console.log(wynik)
+        // }
 
     }
+
+
 
     render() {
         return (
@@ -37,30 +106,36 @@ export default class Kalkulator extends Component {
                 <div className='Kalkulator__conteiner'>
                     <p>Kaklulator dzęmiku</p>
 
-                    <form >
+                    <form onSubmit={this.handleSubmmit}>
                         <label htmlFor='truskawki'>Cena za 1 kg Truskawek:
-                            <input type="number" id='truskawki' name='truskawkicena' value={this.state.truskawkicena} onChange={this.handleChange} />
+                            <input type='number' id='truskawki' name='truskawkicena' value={this.state.truskawkicena} onChange={this.handleChange} />
                         </label>
 
                         <label htmlFor='cukier'>Cena za 1 kg cukru:
-                            <input type="number" id='cukier' name='cukiercena' value={this.state.cukiercena} onChange={this.handleChange} />
+                            <input type='number' id='cukier' name='cukiercena' value={this.state.cukiercena} onChange={this.handleChange} />
                         </label>
 
+                        <label htmlFor='owoce'>Ile kg owoców:
+                            <input type='number' id='owoce' name='owocecena' value={this.state.owocecena} onChange={this.handleChange} />
+                        </label>
+                        <br />
+
                         <label htmlFor='energia'>Cena za 1kWg:
-                            <input type="number" id='energia' name='energiacena' value={this.state.energiacena} onChange={this.handleChange} step={0.01} precision={2} />
+                            <input type='number' id='energia' name='energiacena' value={this.state.energiacena} onChange={this.handleChange} step={0.01} precision={2} />
                         </label>
 
                         <label htmlFor='czas'>Cza przygotowania:
-                            <input type="number" id='czas' name='czascena' value={this.state.czascena} onChange={this.handleChange} step={0.01} precision={2} readOnly />
+                            <input type='number' id='czas' name='czascena' value={this.state.czascena} onChange={this.handleChange} step={0.01} precision={2} readOnly />
                         </label>
                         <label htmlFor='moc'>Moc grzewcza:
-                            <input type="number" id='moc' name='moccena' value={this.state.moccena} onChange={this.handleChange} step={0.01} precision={2} readOnly />
+                            <input type='number' id='moc' name='moccena' value={this.state.moccena} onChange={this.handleChange} step={0.01} precision={2} readOnly />
+                        </label>
+                        <br />
+                        <label htmlFor='sloiki'> Czy masz własne słoiki?
+                            <input id='sloiki' type='checkbox' name='sloiki' checked={this.state.sloiki} onChange={this.handleChange} />
                         </label>
                         <br />
                         <button>Oblicz opłacalność</button>
-
-
-
                     </form>
 
 
